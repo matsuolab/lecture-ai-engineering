2813c486ca5b0d3017f615294c8fc03e3c6ab0cd
kidarikuto <tan04tan20@gmail.com>
2025-04-23 11:01:15 +0000

    [add] 別モデルの参照

--- day1/02_streamlit_app/config.py
+++ day1/02_streamlit_app/config.py
@@ -1,3 +1,6 @@
 # config.py
 DB_FILE = "chat_feedback.db"
 MODEL_NAME = "google/gemma-2-2b-jpn-it"
+# MODEL_NAME = "rinna/japanese-gpt-1b"
+# MODEL_NAME = "rinna/japanese-gpt-neox-3.6b-instruction-sft"
+# MODEL_NAME = "sambanovasystems/SambaLingo-Japanese-Chat"
