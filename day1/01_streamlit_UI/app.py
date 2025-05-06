1cf6bc408d0c5e64f2fc16345b30b4f163eac07b
Yuki97877 <yuki.978788@gmail.com>
2025-05-07 05:06:00 +0900

    Update app.py

--- day1/01_streamlit_UI/app.py
+++ day1/01_streamlit_UI/app.py
@@ -64,14 +64,14 @@ st.write(f"あなたの年齢: {age}")
 # st.header("レイアウト")
 
 # カラム
-# st.subheader("カラムレイアウト")
-# col1, col2 = st.columns(2)
-# with col1:
-#     st.write("これは左カラムです")
-#     st.number_input("数値を入力", value=10)
-# with col2:
-#     st.write("これは右カラムです")
-#     st.metric("メトリクス", "42", "2%")
+st.subheader("カラムレイアウト")
+col1, col2 = st.columns(2)
+with col1:
+    st.write("これは左カラムです")
+    st.number_input("数値を入力", value=10)
+with col2:
+    st.write("これは右カラムです")
+    st.metric("メトリクス", "42", "2%")
 
 # タブ
 # st.subheader("タブ")
