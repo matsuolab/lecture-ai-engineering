6b626e677be3439c75eeadaa77fe188819a1a153
unknown <akihisa_miyakawa@r.recruit.co.jp>
2025-05-19 23:54:36 +0900

    Fix model path for CI environment

--- day5/演習2/test_model.py
+++ day5/演習2/test_model.py
@@ -3,7 +3,7 @@ from sklearn.metrics import accuracy_score
 import pandas as pd
 
 def test_model_accuracy():
-    model = load("models/model_for_ci.pkl")
+    model = load("day5/演習2/models/model_for_ci.pkl")
     X_test = pd.read_csv("data/Titanic.csv").drop("Survived", axis=1)
     y_test = pd.read_csv("data/Titanic.csv")["Survived"]
 
